48b5c767a94428fa677a1d8b333e81feacc97a10
screeeen <miguel@screenimplosion.com>
2019-05-29 16:22:57 +0200

    heroku

---
+++ public/precache-manifest.746fb597811d6071ac9def34281c299f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5943c9a0ce1f10077a7",
+    "url": "/static/js/main.6adbb92e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdb65bcbb355ddaaf6f3",
+    "url": "/static/js/2.f0083912.chunk.js"
+  },
+  {
+    "revision": "31354796a6a0113c4d11317c74510bc6",
+    "url": "/static/media/user.31354796.svg"
+  },
+  {
+    "revision": "e778dbef801d528de5adf6c30647ea9a",
+    "url": "/static/media/court.e778dbef.gif"
+  },
+  {
+    "revision": "509f6c2d77eda766f986aeb8e7450ac7",
+    "url": "/static/media/anim.509f6c2d.gif"
+  },
+  {
+    "revision": "c7f74c8ccda749026cf417cb5edf83b5",
+    "url": "/static/media/back.c7f74c8c.svg"
+  },
+  {
+    "revision": "40b071d38653a855b296128091d2a5b4",
+    "url": "/static/media/badge.40b071d3.svg"
+  },
+  {
+    "revision": "6177bc182269db50a7d620aae2beaed7",
+    "url": "/static/media/pitch.6177bc18.svg"
+  },
+  {
+    "revision": "a6ac849abb3f6fe6faa2eef6c160c104",
+    "url": "/static/media/basket.a6ac849a.svg"
+  },
+  {
+    "revision": "087cb6795beca6664ca017621d374413",
+    "url": "/index.html"
+  }
+];
